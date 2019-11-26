30ab1c3eca8842eea79c3386bdf63d40678c3241
Guy Rotem <guy.rotem@wix.com>
2019-11-27 00:36:38 +0200

    project structure, 1+2 2018

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+
 # adventofcode
 Advent of Code 2019
---
+++ src/main/scala/adventofcode/y2018/AoC2018_2.scala
@@ -0,0 +1,302 @@
+package adventofcode.y2018
+
+object AoC2018_2 extends App {
+
+  def parse(rawInput: String): Seq[String] = {
+    rawInput.split('\n').toSeq.flatMap(trim)
+  }
+
+  def trim(str: String): Option[String] = {
+    val tr = str.trim
+    if (tr.isEmpty) None else Some(tr)
+  }
+
+  val input = parse(AoC2Resources.rawInput)
+
+  def hasN(str: String, n: Int): Boolean = {
+    str.toSeq.sorted.groupBy(x => x).values.exists(_.length == n)
+  }
+
+  println(
+    input.count(hasN(_, 2)) * input.count(hasN(_, 3))
+  )
+
+  assert(input.map(_.length).distinct.length == 1)
+
+  val res = (0 until input.head.length) map { idx =>
+    findDuplicate(
+      input.map(dropIndex(_, idx))
+    ).map(idx -> _)
+  }
+
+  println(res)
+
+  def dropIndex(str: String, idx: Int) = {
+    str.substring(0, idx) ++ str.substring(idx + 1)
+  }
+
+  def findDuplicate(strs: Seq[String]) = {
+    val ds = scala.collection.mutable.Set[String]()
+    strs find { str =>
+      val r = ds.contains(str)
+      ds += str
+      r
+    }
+  }
+}
+
+object AoC2Resources {
+  val rawInput =
+    """
+naosmkcwtdbfivxuphzweraljq
+nvssmicltdbfiyxuphzgeraljq
+nvosmkcwwdbfiyxuphzeeraljx
+nvosmkcqtdbfiyxupkzgeraljw
+qvosmkcwtdbhiyxuphzgeraljh
+nvocqkcktdbfiyxuphzgeraljq
+nvosmhcwtdbfiyxmphzgekaljq
+nvosmkcwtdbfuyxwpszgeraljq
+nvosmocwtcbfiyxupfzgeraljq
+nvosmkcwtdbfiyxubczgeraljv
+nvosmkswtdbfiyxuphzgeruejq
+nlosmkcwtqbfiyxuphzgyraljq
+nvosmkcwtdbficxuphzgwraljk
+nvosmkkwtdbfiyxxphzgeralcq
+vvosmkcetdbfiyxumhzgeraljq
+evosmkcdtdbfiyxuphkgeraljq
+nvosmkvwtdbkiyxuphzgeraejq
+nvoszkcwtdbfimxuphzgeraljb
+nvozmkcwtdbfiyxuphzgrrcljq
+nvosvacwtdbfiyxuphzgeralzq
+nvosmkcwgdofiyxuthzgeraljq
+nvosmkcwasbfiyxuphzgeradjq
+nvosmkcatobfiyxtphzgeraljq
+nvosmkewtdsfiyxuphzgekaljq
+tvormkcwtdbfiyxuphzieraljq
+nvosgkcwtdbfiyxuuhzgeraqjq
+nvosmkcwtdbqiwxuphzgeralvq
+nvosmkcwtybfiydcphzgeraljq
+nvosnkcwtdbfiyxuphzulraljq
+nvosmkcwtdbbiyuupnzgeraljq
+nvosmwcwtdbfiyxuphzneraojq
+nvohlkcwtdbftyxuphzgeraljq
+nvasmkcwbdbfiyiuphzgeraljq
+nvosmkujtdbfiyxuphzgeraljz
+nvosmgcstdbfiyxuphzgeraljd
+nvoswkcwtsbziyxuphzgeraljq
+nvosmmcwtdbfiyxupbzzeraljq
+nvosmkcwtdbfifxulhzgeralji
+nvolmkcwtdbmiyxuphzgeraljv
+lvnsmkcwtdbfiyxuphzzeraljq
+nvqsmkcwtdbfiyxuphageralfq
+nvosmkcwtdmfiyluphzgeralzq
+nvommvcwtdbfiyxupjzgeraljq
+naosmkcwtdbfsyxuphzgsraljq
+avosmkcwtdbfiyxuphzgebafjq
+ndozmkcwtdbfiyxuhhzgeraljq
+nvosmkcwtubfiyxuphooeraljq
+nvosmkcwtdbliyxuphzgmraljx
+nvosmkcuddbfimxuphzgeraljq
+wvosmkzwrdbfiyxuphzgeraljq
+nvosmkcqtdbfiyxupjzgeraijq
+nvosbkcwtdbfiyduphzgeruljq
+yzosmkcntdbfiyxuphzgeraljq
+nvolmkcwtdbfiyxuphugeralfq
+nvrsmkcwtdbjiyxuphzgejaljq
+nvgsmkcwtdbfiyxuphoglraljq
+nvosmkcwtdbfioxuphzgezalhq
+nvosjkcwtdbfipxuphzgekaljq
+nvosmkcwtabfiyxlpazgeraljq
+nvosmkfwtpnfiyxuphzgeraljq
+nvokmbcwtdbeiyxuphzgeraljq
+nvosmkcwtdbfiyxupmzgmlaljq
+nvosmkcwtdhfiykurhzgeraljq
+nvosmkcwwdbfiyxumhzgiraljq
+cvosmscwtdbfikxuphzgeraljq
+nvosmkcwtdnzirxuphzgeraljq
+nvosmscwtdbfiyxuuhbgeraljq
+nvosmkcwtdbfidxpphzgeraajq
+nvosmkcwtdbfiyxuqhzgurcljq
+nvosmkcwtekfiyxrphzgeraljq
+ntosmkcwtpqfiyxuphzgeraljq
+nvosmkcdtdbfhyxsphzgrraljq
+nvolmkkwtdbfiyxuphzgeralgq
+nvosmrcwtdbfiyxuphzgefkljq
+nvoxmkcwtdbfiysuphzeeraljq
+nvjsmkswtdbfiyxuphzqeraljq
+nvosmkcetdbfiyfuphdgeraljq
+nvosmkkwtpbfsyxuphzgeraljq
+nvosdgcwtdbfiyxupyzgeraljq
+nvosmkcwudbfiyzvphzgeraljq
+nvosmkcwtlbfiyxupkzgerzljq
+nvosmkcwtdbfiywuphyzeraljq
+nvocmkcwtdufiyxukhzgeraljq
+nvosmkcwtdqfiyxuphzgevaxjq
+nvosvkcwtdbgiyxuphzgeralzq
+nqosmkcwtdbfiyxuphzeeraljr
+nvosmkcetdbfiyxuphzgeroljo
+nvosmkcwtdvfiyxuphzceraliq
+nvosmkcwtnxfiyxuphzgyraljq
+nvosmkfwtdefiyxupxzgeraljq
+nvosmacwtdbfiyxuphzseragjq
+nvpsmkcwtdbfzyxuvhzgeraljq
+nvormkcwtdbfiyxuphzairaljq
+rvysmkcwtdbfmyxuphzgeraljq
+nvosmscwzdbfiyxuphzgerbljq
+nvosmkcwtdufmyxuphzqeraljq
+nvosmkcwtxbfiyxxphzgeralxq
+nvosmkcwtdbsiyxupsfgeraljq
+nvosmccwtdbfiqxuthzgeraljq
+nvosmtcwtqbuiyxuphzgeraljq
+nvosmkcwtdbfiysurbzgeraljq
+nvowmkcwtdbfiyxuywzgeraljq
+xvosmkcktdbfiyxuhhzgeraljq
+nvosmkgwsdbfiyxmphzgeraljq
+jvofmkcwtdbfiyxupyzgeraljq
+nvozakcwtdbfiexuphzgeraljq
+nvosmkcptdbfiyxuphzgepaljn
+nvosmkcwtdbpiyxuphzgeraxjw
+nvoszkcwtdbfiyjuphzeeraljq
+nvosmkcwtdbfiyxuppzoeraejq
+nvosmkiytdbfiyhuphzgeraljq
+nvosmkcwtdvfiywupyzgeraljq
+nvosmecwtdofiyxuphzgeralja
+nvosmkqwtdbfixxuphzgeraojq
+nvosmkwwtdbfiyxfpdzgeraljq
+nvosmkgwtdbfiyzupwzgeraljq
+nmosmucwtdvfiyxuphzgeraljq
+nvosmdcwtdbmiyxuphzveraljq
+wvosmkcwtpbfiyxuphzgetaljq
+nvosmmcwtdlfbyxuphzgeraljq
+nvosmkcwtabmiexuphzgeraljq
+nvosqpcwtdbfiyxuphzgqraljq
+nvosmecwjdbfiyxuphzgeraljk
+nyohmkcwtdbfiyxuphzgzraljq
+nlosmkcwtkbfiyxuphzgeraejq
+nvosmkcwrdbliyxuphzgerpljq
+nvusmkzwtdbfxyxuphzgeraljq
+nvosmkcwtdbfiyxuhizgerazjq
+nvosmkhptdbfbyxuphzgeraljq
+nvosmfcwtdbgiyxupdzgeraljq
+nvosmkmwtdbfiyxuphzgevalpq
+nvosmkcwtdwfiyxuphzherjljq
+nvosmkcwjwbfiyxuphzgeualjq
+nvosmkcwxdbflymuphzgeraljq
+nvosmkcwpdbriyxuphzoeraljq
+nvoshkcwcdbfiyxuphzgeravjq
+nvosmkcetcbfiyxgphzgeraljq
+nvosmkcwtdyfiyxuphzgerwqjq
+nuosmkcwedbfiyxurhzgeraljq
+nvosmkcwtdbfiixuphzctraljq
+nvoszkcwtdbfwyxuphzgerpljq
+nvormkcwtdbfiyxuphzgeralzn
+nvosmkyttdbfiywuphzgeraljq
+nvosmkcwtdbhiyxupazgeralhq
+nvotmkcwtdbfiyxuphzgevalbq
+nvosmkcwedbfiyxuphzguraljr
+nvssmkcwtdbfiyxushzgeralbq
+nvosmkcwtdeziyxuphzgeralhq
+nvogmkcwtdbfiyxuphzgerrxjq
+ncormkcwtdbfiyxuphzgeraloq
+nvosmkcwbdbfiyeuphzgerqljq
+nvosxkcwtdbfsyxupfzgeraljq
+nvohmkcwtdbfiyxuphzseraajq
+nvoscdcwtdbfiyxuphzgeralqq
+neosmkcwtdbfiyxuchzgeralgq
+njosmvcwpdbfiyxuphzgeraljq
+nvosmkcwtwbfiyxuphzgehamjq
+nvosmkcwtdbfiyxushzgejaljv
+nvosmkcwodbfiyxuphzgeryqjq
+nvoymqcwtdbfiyxuphzgeralbq
+nvosmkcwtdjfiyxuphzgesaljb
+nvjsmdcwedbfiyxuphzgeraljq
+nvosmkcwydbfiyxuihzmeraljq
+nvrsmkcwtdifiyxuphzgqraljq
+nposmkcwtdbfiyxiohzgeraljq
+dvosmkcwtdbfiyxuphzrvraljq
+pvosmkcwudbfsyxuphzgeraljq
+noosmkcwtdbfiyxuphtgexaljq
+nvosmkcwtdbfiaxuphyferaljq
+nvhsmlcwtdbfiyxuphzgeualjq
+nvosekcwtdbbiyxuphzgerabjq
+nvosvkcitdbfiyxuphzgerarjq
+nvotmkkwtdbfiyxuphzgeraljj
+nvosmecwtdbfiyxuphzgyralwq
+hvosmkcwtdbfiyxuphzysraljq
+nvosmkcvtdbfiyxlphzgeraljb
+nvosmkcwttbfiyxuphngtraljq
+nvoslkcwtdbfiyxuphzqeraljr
+nxosmkcwtdbfibxuphzgrraljq
+nvokmkhwtdbfiyxuphzgwraljq
+nvosmkfwtdbfiyxuphzgdraljo
+nvcsmkcwtdbfibxuphzgeraljl
+nvosmkcwtdcfiaxuphzeeraljq
+wvosmkcwtdbyiyxjphzgeraljq
+nyosmbcwtjbfiyxuphzgeraljq
+nvosmkcwtdbiiyxuahzieraljq
+nqosmkcwtdbfiyxuyhzgerapjq
+nvosmkcwtdbfiyxuwhzzetaljq
+nvosmkcwfgbfiyxuphzgerrljq
+nvosmbcwtdbfipxuphzderaljq
+nvosmkcwtdgfiyxupdzgerjljq
+noosmkcwtdcfiyxuphlgeraljq
+nvonmkcutdbfiyxuphzieraljq
+nvocmkcwtdbfiyyuphageraljq
+nvosmkcwtdbfoyxuphzneraqjq
+nvoskkcwtdbtiyxuphzgevaljq
+ocosmkswtdbfiyxuphzgeraljq
+nvosmkcqtdbfiyxfvhzgeraljq
+noosmkcwtdbfiyquphzberaljq
+nvosmkcwttbfijxuchzgeraljq
+nvogmkcwtdbfiyxupazgeralaq
+nvqsmkcwtdbfikxuphzgeraliq
+nvosmkuwtdbfiyxuphzjwraljq
+nyosmhcwtdbfiyxuphzgereljq
+nvosmncwtdbfietuphzgeraljq
+gvpsmkcwtdbfiyxuyhzgeraljq
+nvozmkewtlbfiyxuphzgeraljq
+nvostkcltpbfiyxuphzgeraljq
+nvosmkcwtdbdiyxuphzgehaljz
+nvosmkcwtjbziyxuphzgexaljq
+nvosmkcwtdbfiyptphzggraljq
+nvosmkcwtdbliyxupjzgebaljq
+nvosmkawtdbfiyxupxzgtraljq
+vvosmkcwtdbfiyxfphzperaljq
+nvosmkawtdbfiyxutczgeraljq
+nvosmkcbtdbuiyxrphzgeraljq
+nvbsmkcwtdbfiyxdphzgerasjq
+nvosnkcwqdsfiyxuphzgeraljq
+nvosmkcwtdbfiyxwphzgzzaljq
+nvosmkcwtdbffyquphzgeralcq
+nvosmkcwtzbfiyxdphzgzraljq
+nvysmkcwtdbfiycvphzgeraljq
+nvowmkcwtdbfiycuyhzgeraljq
+nvosbkcwtdbfiyiuphzgeraqjq
+nvosmecwtdbfiyxupqzmeraljq
+nvosmkcdtdbfhyxsphzgeraljq
+nmosmkcwtdbziyxuphzgercljq
+nvosmkcwtdbfiyxupfmgersljq
+nvosmkcvtdbpyyxuphzgeraljq
+nvosmkcwtkbfiyaupxzgeraljq
+nvosmkcwtzbiiyxuphzgerazjq
+nvoxmkcwtdbfiyxuphztegaljq
+nvonmkcwtdafiyxuphzgerkljq
+rvommkcwtdbfiyxzphzgeraljq
+nvosmkcwthbfiysuphzgeraxjq
+nvosmkcwtdbfnyxuphzgerccjq
+nrosmzcwtdbfiyxuphkgeraljq
+nvolmkcdtdbfiyxuphtgeraljq
+nvosfkcwtdbfiyeuphcgeraljq
+nvowmkcwtdbfhyxuphzgerafjq
+gvosmkcwtdbfiyxupbpgeraljq
+nvosmkcwtdbkiyxuphegebaljq
+nvommufwtdbfiyxuphzgeraljq
+uvksmkcwtdbfiysuphzgeraljq
+nvosmkcwevbfiyxuphtgeraljq
+nvosmkcmtdbfiycuphzgeraxjq
+nvcsxkcwtdbfiyxuphzgeraljn
+nvosmkcwtdbtiymuphzgeraltq
+nvosmfcwtdlfjyxuphzgeraljq
+svosmkcitdbfiyxuphzgsraljq
+    """
+}
